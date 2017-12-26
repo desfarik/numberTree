055f85e44f0460ea318166440f47d4a4b6c5f040
desfarik <desfarik@gmail.com>
2017-12-26 21:59:48 +0300

    refactoring

--- source/components/main.module.js
+++ source/components/main.module.js
@@ -3,10 +3,10 @@ import headerDirective from './header/header.directive';
 import inputDirective from './input/input.directive';
 import numberExpressionValidator from './input/service/number-expression.validator.service';
 import numberExpressionTransformer from './input/service/number-expression.transformer';
-import numberExpressionConstants from './input/constants/number-expression.constants';
-import numberExpressionParser from './service/number-expression.parser.service';
-import polishExpressionParser from './service/polish-expression.parser.service';
-import treeExpressionParser from './service/tree-expression.parser.service';
+import numberExpressionConstants from './constants/number-expression.constants';
+import numberExpressionParser from './services/number-expression.parser.service';
+import polishExpressionParser from './services/polish-expression.parser.service';
+import treeExpressionParser from './services/tree-expression.parser.service';
 
 export default angular.module('mainPage', ['ngMessages'])
     .config(mainRouter)

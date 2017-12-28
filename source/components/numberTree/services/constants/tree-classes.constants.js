78218cac8c070a2aec35a6c86d8fffd992ce02b0
desfarik <desfarik@gmail.com>
2017-12-28 22:33:03 +0300

    add line with arrow from children to parent

--- source/components/numberTree/services/constants/tree-classes.constants.js
+++ source/components/numberTree/services/constants/tree-classes.constants.js
@@ -5,4 +5,5 @@ export default {
     CHILD_PAIR_CLASS: "child-pair",
     PAIR_OPERAND_CLASS: "pair-operand",
     TREE_WRAPPER_ELEMENT_ID: 'tree-wrapper',
+    TREE_CONNECTIONS_CONTAINER: 'lines-container',
 }
